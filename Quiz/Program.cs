c3ff252dd4b538c5e9b82a3ea1e3a55caec12de8
Ryan Capurso <ryan_capurso@hotmail.com>
2023-07-31 12:22:30 -0300

    Refatorando código

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,44 +1,64 @@
 ﻿int pontos = 0;
-Console.WriteLine("===================");
-Console.WriteLine("Seja bem vindo !!");
-Console.WriteLine("===================");
-
-Console.WriteLine("Aqui está nossos temas para jogar");
-Console.WriteLine("1 - Jogos");
-Console.WriteLine("2 - Filmes");
-Console.WriteLine("3 - Séries");
-Console.WriteLine("0 - Sair");
-
-Console.Write("Selecione o tema do Quiz que deseja jogar: ");
-string opcaoEscolhida = Console.ReadLine()!;
-
-int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida); 
-
-
-switch (opcaoEscolhidaNumerica)
+string opcaoEscolhida;
+ExibirMenu();
+void ExibirMenu()
 {
-	case 1:
-        Console.WriteLine("Tema Jogos foi selecionado");
-        TemaJogos();
-        break;
-    case 2:
-        Console.WriteLine("Tema Filmes foi selecionado");
-        break;
-        case 3:
-        Console.WriteLine("Tema Séries foi selecionado");
-        break;
-    case 0:
-        Console.WriteLine("Até a próxima");
-        Environment.Exit(0);
-        break;
-    default:
-        Console.WriteLine("Error ! Digite uma opção válida");
-        break;
-}
+    Console.WriteLine("===================");
+    Console.WriteLine("Seja bem vindo !!");
+    Console.WriteLine("===================");
 
+    Console.WriteLine("Aqui está nossos temas para jogar");
+    Console.WriteLine("1 - Jogos");
+    Console.WriteLine("2 - Filmes");
+    Console.WriteLine("3 - Séries");
+    Console.WriteLine("0 - Sair");
+
+    Console.Write("Selecione o tema do Quiz que deseja jogar: ");
+    opcaoEscolhida = Console.ReadLine()!;
+
+    switch (ConverteOpcaoEscolhida(opcaoEscolhida))
+    {
+        case 1:
+            Console.WriteLine("Tema Jogos foi selecionado");
+            TemaJogos();
+            break;
+        case 2:
+            Console.WriteLine("Tema Filmes foi selecionado");
+            break;
+        case 3:
+            Console.WriteLine("Tema Séries foi selecionado");
+            break;
+        case 0:
+            Console.WriteLine("Até a próxima");
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Error ! Digite uma opção válida");
+            break;
+    }
+}
 int ConverteOpcaoEscolhida(string opcaoTexto)
 {
     return int.Parse(opcaoEscolhida);
+}
+void VerificaResposta(int indiceResposta)
+{
+    Console.Write("\nSua resposta: ");
+    opcaoEscolhida = Console.ReadLine()!;
+
+    if (ConverteOpcaoEscolhida(opcaoEscolhida) == indiceResposta)
+    {
+        Console.WriteLine("Resposta Correta!");
+        ++pontos;
+        ExibirPontuação();
+    }
+    else
+    {
+        Console.WriteLine("Resposta Incorreta!");
+        ExibirPontuação();
+    }
+    Thread.Sleep(1000);
+    Console.Clear();
 }
 void ExibirPontuação()
 {
@@ -54,74 +74,22 @@ void TemaJogos()
     Console.WriteLine("3 - Resposta Certa");
     Console.WriteLine("4 - Resposta Errada");
 
-    Console.Write("Sua resposta: ");
+    VerificaResposta(3);
 
-    opcaoEscolhida = Console.ReadLine()!;
-
-    if (ConverteOpcaoEscolhida(opcaoEscolhida) == 3)
-    {
-        Console.WriteLine("Resposta Correta!");
-        ++pontos;
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-    }
-    else
-    {
-        Console.WriteLine("Resposta Incorreta!");
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-    }
-
-    Console.Clear();
     Console.WriteLine("Pergunta 2");
     Console.WriteLine("1 - Respota Errada");
     Console.WriteLine("2 - Resposta Certa");
     Console.WriteLine("3 - Resposta Errada");
     Console.WriteLine("4 - Resposta Errada");
 
-    Console.Write("Sua resposta: ");
-    opcaoEscolhida = Console.ReadLine()!;
+    VerificaResposta(2);
 
-    opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-
-    if (opcaoEscolhidaNumerica == 2)
-    {
-        Console.WriteLine("Resposta Correta!");
-        ++pontos;
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-    }
-    else
-    {
-        Console.WriteLine("Resposta Incorreta!");
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-    }
-
-    Console.Clear();
     Console.WriteLine("Pergunta 2");
     Console.WriteLine("1 - Respota Errada");
     Console.WriteLine("2 - Resposta Errada");
     Console.WriteLine("3 - Resposta Errada");
     Console.WriteLine("4 - Resposta Certa");
 
-    Console.Write("Sua resposta: ");
-    opcaoEscolhida = Console.ReadLine()!;
+    VerificaResposta(4);
 
-    opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-
-    if (opcaoEscolhidaNumerica == 4)
-    {
-        Console.WriteLine("Resposta Correta!");
-        ++pontos;
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-    }
-    else
-    {
-        Console.WriteLine("Resposta Incorreta!");
-        Console.WriteLine("Pontuação");
-        Console.WriteLine($"{pontos} / 3");
-
-    }
 }
